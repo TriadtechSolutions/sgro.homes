ae8c4474dfc708005e882e0dbb733811c1d4e31f
Muthukumar s <muthu.triadtechsolutions@gmail.com>
2026-09-02 00:14:26 +0530

    menu updated

--- web/themes/custom/homes/js/global.js
+++ web/themes/custom/homes/js/global.js
@@ -1,15 +1,58 @@
 /**
  * @file
- * Global utilities.
- *
+ * Global utilities for Homes theme.
  */
 (function (Drupal) {
 
   'use strict';
 
-  Drupal.behaviors.homes = {
-    attach: function (context, settings) {
+  Drupal.behaviors.homesJumpNav = {
+    attach: function (context) {
+      const toggler = context.querySelector('#homes-mobile-toggler');
+      const navbar = document.querySelector('#HomesCollapsingNavbar');
 
+      if (toggler && navbar && !toggler.dataset.jumpModeAttached) {
+        toggler.dataset.jumpModeAttached = "true";
+
+        const iconOpen = toggler.querySelector('.hamburger-icon-open');
+        const iconClose = toggler.querySelector('.hamburger-icon-close');
+
+        function toggleMenu(showState) {
+          const isShown = navbar.classList.contains('show') || navbar.classList.contains('is-open');
+          const shouldShow = showState !== undefined ? showState : !isShown;
+
+          if (shouldShow) {
+            navbar.classList.add('show', 'is-open');
+            navbar.classList.remove('collapsing');
+            toggler.classList.add('is-open');
+            toggler.setAttribute('aria-expanded', 'true');
+            if (iconOpen) iconOpen.style.display = 'none';
+            if (iconClose) iconClose.style.display = 'block';
+          } else {
+            navbar.classList.remove('show', 'is-open', 'collapsing');
+            toggler.classList.remove('is-open');
+            toggler.setAttribute('aria-expanded', 'false');
+            if (iconOpen) iconOpen.style.display = 'block';
+            if (iconClose) iconClose.style.display = 'none';
+          }
+        }
+
+        toggler.addEventListener('click', function (e) {
+          e.preventDefault();
+          e.stopPropagation();
+          toggleMenu();
+        });
+
+        // Close menu when clicking menu link targets on mobile
+        const menuLinks = navbar.querySelectorAll('a');
+        menuLinks.forEach(function (link) {
+          link.addEventListener('click', function () {
+            if (window.innerWidth < 992) {
+              toggleMenu(false);
+            }
+          });
+        });
+      }
     }
   };
 
